3337940a4cb3eacc3b4b40a111e257bbcd9d13c9
Georgios Konstantopoulos <me@gakonst.com>
2018-07-03 22:09:24 +0300

    Add ERC20/ETH/ERC721 withdrawals

--- server/contracts/Core/RootChain.sol
+++ server/contracts/Core/RootChain.sol
@@ -1,18 +1,20 @@
 pragma solidity ^0.4.24;
 
-// Zeppelin Imports
+
+// ERC721
 import "openzeppelin-solidity/contracts/token/ERC721/ERC721.sol";
 import "openzeppelin-solidity/contracts/token/ERC721/ERC721Receiver.sol";
-import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 
-// ERC20 Receiver
+// ERC20
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 import "./ERC20Receiver.sol";
-
 
 // Lib deps
 import "../Libraries/Transaction/Transaction.sol";
 import "../Libraries/ECVerify.sol";
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 
+// SMT and VMC
 import "./SparseMerkleTree.sol";
 import "./ValidatorManagerContract.sol";
 
@@ -92,6 +94,17 @@ contract RootChain is ERC721Receiver, ERC20Receiver {
      * @param amount The bond amount which has been withdrawn
      */
     event WithdrewBonds(address indexed from, uint256 amount);
+
+    /**
+     * Event to log the withdrawal of a coin
+     * @param from The address of the user who withdrew bonds
+     * @param mode The type of coin that is being withdrawn (ERC20/ERC721/ETH)
+     * @param contractAddress The contract address where the coin is being withdrawn from
+              is same as `from` when withdrawing a ETH coin
+     * @param uid The uid of the coin being withdrawn if ERC721, else 0
+     * @param denomination The denomination of the coin which has been withdrawn (=1 for ERC721)
+     */
+    event Withdrew(address indexed from, Mode mode, address contractAddress, uint uid, uint denomination);
 
     using SafeMath for uint256;
     using Transaction for bytes;
@@ -397,8 +410,24 @@ contract RootChain is ERC721Receiver, ERC20Receiver {
     /// @param slot The slot of the coin being withdrawn
     function withdraw(uint64 slot) external isState(slot, State.EXITED) {
         require(coins[slot].owner == msg.sender, "You do not own that UTXO");
-        ERC721(coins[slot].contractAddress).safeTransferFrom(address(this), msg.sender, uint256(coins[slot].uid));
-    }
+        uint256 uid = coins[slot].uid;
+        uint256 denomination = coins[slot].denomination;
+
+		// Delete the coin that is being withdrawn
+        Coin memory c = coins[slot];
+        delete coins[slot];
+        if (c.mode == Mode.ETH) {
+            msg.sender.transfer(denomination);
+        } else if (c.mode == Mode.ERC20) {
+            ERC20(c.contractAddress).transfer(msg.sender, denomination);
+        } else if (c.mode == Mode.ERC721) {
+            ERC721(c.contractAddress).safeTransferFrom(address(this), msg.sender, uid);
+        } else {
+            revert('Invalid coin mode');
+        }
+
+        emit Withdrew(msg.sender, c.mode, c.contractAddress, uid, denomination);
+}
 
     /******************** CHALLENGES ********************/
 
@@ -663,7 +692,7 @@ contract RootChain is ERC721Receiver, ERC20Receiver {
             if (exitSlots[i] == slot)
                 return i;
         }
-        // a defualt value to return larger than the possible number of coins
+        // a default value to return larger than the possible number of coins
         return 2**65;
     }
 
