b75922fbdf74bbb3b8e7017d88c366b11095eb46
yace132 <10000reset@gmail.com>
2018-07-26 14:54:40 +0800

    Update SparseMerkleTree.sol

--- server/contracts/Core/SparseMerkleTree.sol
+++ server/contracts/Core/SparseMerkleTree.sol
@@ -11,6 +11,7 @@ contract SparseMerkleTree {
         // defaultHash[0] is being set to keccak256(uint256(0));
         defaultHashes[0] = 0x290decd9548b62a8d60345a988386fc84ba6bc95484008f6362f93160ef3e563;
         setDefaultHashes(1, DEPTH);
+        // 在合約創造時先算好defaulthashes
     }
 
     function checkMembership(
@@ -31,14 +32,17 @@ contract SparseMerkleTree {
         uint16 p = 8;
         uint64 proofBits;
         assembly {proofBits := div(mload(add(proof, 32)), exp(256, 24))}
+        //proofBits用一連串0,1表示merkle tree路徑上的節點是否為空
+        //可參考 https://ethresear.ch/t/plasma-cash-with-sparse-merkle-trees-bloom-filters-and-probabilistic-transfers/2006
+        
 
         for (uint d = 0; d < DEPTH; d++ ) {
             if (proofBits % 2 == 0) { // check if last bit of proofBits is 0
-                proofElement = defaultHashes[d];
+                proofElement = defaultHashes[d]; //空的節點用defaultHash
             } else {
                 p += 32;
                 require(proof.length >= p);
-                assembly { proofElement := mload(add(proof, p)) }
+                assembly { proofElement := mload(add(proof, p)) } //有東西的節點就去讀hash值
             }
             if (index % 2 == 0) {
                 computedHash = keccak256(abi.encodePacked(computedHash, proofElement));
@@ -46,7 +50,7 @@ contract SparseMerkleTree {
                 computedHash = keccak256(abi.encodePacked(proofElement, computedHash));
             }
             proofBits = proofBits / 2; // shift it right for next bit
-            index = index / 2;
+            index = index / 2; //index代表一路上走左邊或右邊，在sparse merkle tree裡，即slot，coin的真正id
         }
         return computedHash;
     }
